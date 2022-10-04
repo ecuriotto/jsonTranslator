c2e92dab38b50cbf14955ddb0cc0150510d11733
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2022-10-04 11:12:54 +0300

    flat solution

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+
+var app = express();
+app.set('view engine','ejs');
+
+app.get('/', function (req, res) {
+
+  const mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  const tagline = "No programming concept is complete without a cute animal mascot.";
+
+  //res.send('Simple Web Application is UP 2');
+  res.render('index', {
+    mascots: mascots,
+    tagline: tagline
+  });
+
+});
+
+app.listen(7070, function () {
+
+console.log('Simple Web Application running on port 7070!');
+
+});
