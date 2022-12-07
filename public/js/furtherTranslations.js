ecd2920c83d6d9b06ea070549730ebcf31991262
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2022-12-07 17:55:08 +0200

    snackbar added

--- public/js/furtherTranslations.js
+++ public/js/furtherTranslations.js
@@ -4,6 +4,15 @@ const savePrevPhrases = (file) => {
     const myDataRaw = event.target.result;
 
     await makeRequest('POST', '/savePreviousVersionTrans/' + languageSelection.code, myDataRaw);
+    let x = document.getElementById('snackbar');
+
+    // Add the "show" class to DIV
+    x.className = 'show';
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () {
+      x.className = x.className.replace('show', '');
+    }, 3000);
   });
   reader.readAsText(file);
 };
